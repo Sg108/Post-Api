fd38e06f8702b332b13855c80da3bd06e0a9d23c
Shivam <guptashivamgzb@gmail.com>
2024-03-08 18:33:23 +0530

    login added

--- index.js
+++ index.js
@@ -92,7 +92,53 @@ app.get('/posts',verifyToken,(req, res) => {
     res.json(arr)
     }
 })
+app.post("/login", async (req, res) => {
+ try{
+   const user = await User.findOne({email:req.body.email})
+   if(!user)
+   {
+    return res.status(401).json({message:'User not found'})
+   } 
+   else{
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+  )
 
+  const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+
+  const inputPassword = req.body.password
+
+  if(originalPassword != inputPassword)
+  { 
+      //console.log("Wrong password")
+  return res.status(401).json({message:'Incorrect password'})
+  }
+
+  const accessToken = jwt.sign(
+      {
+          id: user._id,
+         // isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" }
+  )
+  console.log(accessToken)
+  const { password, ...others } = user._doc
+  //console.log(password)
+ 
+
+  //res.setHeader('Set-Cookie','jwt='+accessToken+";path=/;SameSite=None;Secure")
+
+  res.status(200).json({ ...others, accessToken })
+  
+}
+ }
+catch(e){
+  res.status(500).json('login failed')
+}
+   
+})
 app.post("/register", async (req, res) => {
   console.log(req.body)
   const newUser = new User({
